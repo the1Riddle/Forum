20f2b4f219f4a056aa8593ff11abd42704bba7df
elvisotieno0 <elvisotienomboya@gmail.com>
2026-04-29 11:16:08 +0000

    clean the stracture (#1)
    Reviewed-on: http://learn.zone01kisumu.ke/git/aokutu/Forum/pulls/1
    Co-authored-by: elvisotieno0 <elvisotienomboya@gmail.com>
    Co-committed-by: elvisotieno0 <elvisotienomboya@gmail.com>

--- Jwt/go.mod
+++
@@ -1,5 +0,0 @@
-module JsonWebToken
-
-go 1.24.3
-
-require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
--- session/session.go
+++
@@ -1,5 +0,0 @@
-package session
-
-import "github.com/gorilla/sessions"
-
-var Store = sessions.NewCookieStore([]byte("secret-key"))
